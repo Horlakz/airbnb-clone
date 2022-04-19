164c92f336a3297aedee931d09dfe3310174ab07
Horlakz <horlakz@protonmail.com>
2022-04-20 00:05:02 +0100

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("user-icon").addEventListener('click', showDropDown)
+
+const dropDown = document.getElementById('drop-down')
+
+function showDropDown() {
+
+    // dropDown.style.display = 'none'
+
+    if (dropDown.style.display = 'none') {
+        dropDown.style.display = 'block'
+    } else if () {
+        dropDown.style.display = 'none'
+    }
+}
